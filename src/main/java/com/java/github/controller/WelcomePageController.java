ffb9908fa4234b46bc2ba1ae74bb2cb1ca0bfdcd
Sadhanas <Sadhanas@LAPTOP-0UJER1DL>
2021-05-15 22:09:20 +0530

    Circular view path [welcome] issue resolved

--- src/main/java/com/java/github/controller/WelcomePageController.java
+++ src/main/java/com/java/github/controller/WelcomePageController.java
@@ -3,19 +3,21 @@ package com.java.github.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /****************************************************************************************
  * This controller is the main page of the Heroku Java UI 
  * This will be called everytime the Heroku Java webapp is loaded
  ****************************************************************************************/
 @Controller
+@RequestMapping("/")
 public class WelcomePageController {
 	public static String CURRENT_CLASS		 		= "WelcomePageController";
 	/**
 	 * This method gets called when the main page of the Heroku GDPR UI is called 
 	 * @return  
 	 */
-	@RequestMapping(value = {"/welcome", "/"})
+	@RequestMapping(method = RequestMethod.GET, produces = "text/html")
 	public String welcome(Model model) {
 		String CURRENT_METHOD = "welcome";		
 		System.out.println(CURRENT_CLASS+" ::: "+CURRENT_METHOD+":: Inside method");
